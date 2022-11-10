f6a1f277d1cb33180155aca479d93cf1d259f4a1
d_rainey19 <drainey19@georgefox.edu>
2022-11-10 15:33:44 -0800

    added initial javadoc

--- StackCalc.java
+++ StackCalc.java
@@ -1,5 +1,11 @@
 import java.util.Stack;
 
+/**
+ * Stack-based command-line utility program
+ *
+ * @author drainey19@georgefox.edu
+ * @author blee20@georgefox.edu
+ */
 public class StackCalc {
     public static void main(String[] args) {
         // TODO implement program
